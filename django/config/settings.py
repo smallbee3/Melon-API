48d387fced961af8e9cc54aa8933794128b4f219
LeeHanYeong <dev@azelf.com>
2018-02-06 16:30:08 +0900

    album, artist, song app추가

--- django/config/settings.py
+++ django/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'album',
+    'artist',
+    'song',
 ]
 
 MIDDLEWARE = [
