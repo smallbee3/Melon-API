b91dba83d3fef99190c0389e8f31d021d154324d
LeeHanYeong <dev@azelf.com>
2018-02-19 10:56:45 +0900

    song_search url, view, template연결

--- django/song/urls.py
+++ django/song/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 app_name = 'song'
 urlpatterns = [
-    path('', views.song_list, name='song-list'),
+    path('',
+         views.song_list,
+         name='song-list'),
+    path('search/',
+         views.song_search,
+         name='song-search'),
 ]
--- django/song/views.py
+++ django/song/views.py
@@ -13,3 +13,22 @@ def song_list(request):
         'song/song_list.html',
         context,
     )
+
+
+def song_search(request):
+    """
+    사용할 URL: song/search/
+    사용할 Template: templates/song/song_search.html
+        form안에
+            input한개, button한개 배치
+
+    1. song/urls.py에 URL작성
+    2. templates/song/song_search.html작성
+        {% extends %} 사용할 것
+        form배치 후 method는 POST, {% csrf_token %}배치
+    3. 이 함수에서 return render(...)
+        *아직 context는 사용하지 않음
+    :param request:
+    :return:
+    """
+    return render(request, 'song/song_search.html')
