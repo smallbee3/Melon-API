6f7d3c3f10adaf47b0c67bf76fb2391d3727fddc
LeeHanYeong <dev@azelf.com>
2018-02-20 14:53:54 +0900

    artist_search_from_melon의 검색기능 구현

--- django/artist/views.py
+++ django/artist/views.py
@@ -76,11 +76,29 @@ def artist_search_from_melon(request):
     :return:
     """
     keyword = request.GET.get('keyword')
+    context = {}
     if keyword:
+        import re
         import requests
         from bs4 import BeautifulSoup
+        artist_info_list = []
         URL = 'https://www.melon.com/search/artist/index.htm'
-        params = {
-            'q': keyword,
-        }
-    return render(request, 'artist/artist_search_from_melon.html')
+        params = {'q': keyword}
+        response = requests.get(URL, params)
+        soup = BeautifulSoup(response.text, 'lxml')
+        for li in soup.select('div.list_atist12.d_artist_list > ul > li'):
+            dl = li.select_one('div.atist_info > dl')
+            href = li.select_one('a.thumb').get('href')
+            p = re.compile(r"goArtistDetail\('(\d+)'\)")
+
+            name = dl.select_one('dt:nth-of-type(1) > a').get_text(strip=True)
+            url_img_cover = li.select_one('a.thumb img').get('src')
+            artist_id = re.search(p, href).group(1)
+
+            artist_info_list.append({
+                'name': name,
+                'url_img_cover': url_img_cover,
+                'artist_id': artist_id,
+            })
+        context['artist_info_list'] = artist_info_list
+    return render(request, 'artist/artist_search_from_melon.html', context)
