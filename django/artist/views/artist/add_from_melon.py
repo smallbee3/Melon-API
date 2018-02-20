a844f67c4e85ec4ddefc71903ae074f41e9acffa
LeeHanYeong <dev@azelf.com>
2018-02-20 17:05:12 +0900

    주석추가

--- django/artist/views/artist/add_from_melon.py
+++ django/artist/views/artist/add_from_melon.py
@@ -42,9 +42,9 @@ def artist_add_from_melon(request):
     """
     if request.method == 'POST':
         artist_id = request.POST['artist_id']
+        # 크롤링 코드. 자신이 작성한 크롤러를 사용해서 같은 데이터를 가져오면 됩니다
         artist = ArtistData(artist_id)
         artist.get_detail()
-
         name = artist.name
         real_name = artist.personal_information.get('본명', '')
         nationality = artist.personal_information.get('국적', '')
@@ -52,11 +52,15 @@ def artist_add_from_melon(request):
         constellation = artist.personal_information.get('별자리', '')
         blood_type = artist.personal_information.get('혈액형', '')
 
+        # 튜플의 리스트를 순회하며 blood_type을 결정
         for short, full in Artist.CHOICES_BLOOD_TYPE:
             if blood_type.strip() == full:
                 blood_type = short
                 break
         else:
+            # break가 발생하지 않은 경우
+            # (미리 정의해놓은 혈액형 타입에 없을 경우)
+            # 기타 혈액형값으로 설정
             blood_type = Artist.BLOOD_TYPE_OTHER
 
         # artist_id가 melon_id에 해당하는 Artist가 이미 있다면
