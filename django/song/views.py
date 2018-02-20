f6e9de5737f9f6f0f83c029044e70cf1d006d333
LeeHanYeong <dev@azelf.com>
2018-02-20 12:57:11 +0900

    NamedTuple사용

--- django/song/views.py
+++ django/song/views.py
@@ -1,3 +1,5 @@
+from typing import NamedTuple
+
 from django.db.models import Q
 from django.shortcuts import render
 
@@ -49,35 +51,30 @@ def song_search(request):
     :param request:
     :return:
     """
-    # Song과 연결된 Artist의 name에 keyword가 포함되는 경우
-    # Song과 연결된 Album의 title에 keyword가 포함되는 경우
-    # Song의 title에 keyword가 포함되는 경우
-    #   를 모두 포함(or -> Q object)하는 쿼리셋을 'songs'에 할당
-
-    # songs_from_artists
-    # songs_from_albums
-    # songs_from_title
-    #  위 세 변수에 더 위의 조건 3개에 부합하는 쿼리셋을 각각 전달
-    #  세 변수를 이용해 검색 결과를 3단으로 분리해서 출력
-    #  -> 아티스트로 검색한 노래 결과, 앨범으로 검색한 노래 결과, 제목으로 검색한 노래 결과
     context = {
         'song_infos': [],
     }
     keyword = request.GET.get('keyword')
+
     if keyword:
+        class SongInfo(NamedTuple):
+            type: str
+            q: Q
+
         song_infos = (
-            ('아티스트명', Q(album__artists__name__contains=keyword)),
-            ('앨범명', Q(album__title__contains=keyword)),
-            ('노래제목', Q(title__contains=keyword)),
+            SongInfo(
+                type='아티스트명',
+                q=Q(album__artists__name__contains=keyword)),
+            SongInfo(
+                type='앨범명',
+                q=Q(album__title__contains=keyword)),
+            SongInfo(
+                type='노래제목',
+                q=Q(title__contains=keyword)),
         )
         for type, q in song_infos:
             context['song_infos'].append({
                 'type': type,
                 'songs': Song.objects.filter(q),
             })
-
-
-
-
-    context['type'] = 'ASDF'
     return render(request, 'song/song_search.html', context)
