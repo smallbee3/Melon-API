0dec438ea010e379482f1a0ed3d594880ac9fe0a
LeeHanYeong <dev@azelf.com>
2018-02-22 15:36:22 +0900

    header, content-block css구현

--- app/config/urls.py
+++ app/config/urls.py
@@ -18,10 +18,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # artist/로 시작하는 path가
     # artist.urls모듈을 include하도록 설정
+    path('', views.index, name='index'),
     path('artist/', include('artist.urls')),
     path('album/', include('album.urls')),
     path('song/', include('song.urls')),
