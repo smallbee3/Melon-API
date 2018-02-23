1c3329381bb9bd36ed213c6b98eeb690639b5388
LeeHanYeong <dev@azelf.com>
2018-02-23 16:52:06 +0900

    SignupForm에서 각 field에 특정한 validation함수 구현

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+User = get_user_model()
 
 __all__ = (
     'SignupForm',
@@ -17,3 +21,16 @@ class SignupForm(forms.Form):
         label='비밀번호 확인',
         widget=forms.PasswordInput
     )
+
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        if User.objects.filter(username=data).exists():
+            raise ValidationError('이미 사용중인 아이디입니다')
+        return data
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password']
+        password2 = self.cleaned_data['password2']
+        if password1 != password2:
+            raise ValidationError('비밀번호와 비밀번호 확인란의 값이 다릅니다')
+        return password2
